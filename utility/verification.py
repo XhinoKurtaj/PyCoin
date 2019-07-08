8097b63eedb71b3ed98bb83ec0a5581f25321e16
XhinoKurtaj <xhino_kurtaj@yahoo.com>
2019-07-08 10:33:49 +0200

    Initial commit

---
+++ utility/verification.py
@@ -0,0 +1,44 @@
+"""Provides verification helper methods """
+import utility.hash_util as hu
+from wallet import Wallet
+
+class Verification:
+
+    @staticmethod
+    def valid_proof( transactions, last_hash, proof):
+        guess = (str([tx.to_ordered_dict() for tx in transactions]) +
+             str(last_hash) + str(proof)).encode()
+        guess_hash = hu.hash_string_256(guess)
+        return guess_hash[0:2] == '00'
+
+    @classmethod
+    def verify_chain(cls, blockchain):
+        """ Verify the current blockchain and return True if it's valid, False """
+        for (index, block) in enumerate(blockchain):
+            if index == 0:
+                continue
+            if block.previous_hash != hu.hash_block(blockchain[index-1]):
+                return False
+            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+                print('Proof of work is invalid')
+                return False
+        return True
+
+    @staticmethod
+    def verify_transaction( transaction, get_balance, check_funds=True):
+        """Verify a transaction by checking whether the sender has sufficient coins.
+
+        Arguments:
+            :transaction: The transaction that should be verified.
+        """
+        if check_funds:
+            sender_balance = get_balance(transaction.sender)
+            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+        else:
+            return Wallet.verify_transaction(transaction)
+
+
+    @classmethod
+    def verify_transactions(cls, open_transactions, get_balance):
+        """Verifies all open transactions."""
+        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transactions])
